1ec4c4cf559af8bc01984ea65852ebff4f0fb6ac
Nisgames <jannis.wilms@gmail.com>
2024-07-17 15:58:29 +0200

    added JSON endpoint

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import randomname
 import redis
+import string
 
 app = Flask(__name__)
 
@@ -9,6 +10,12 @@ redis = redis.Redis(host='redis', port=6379, decode_responses=True)
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+@app.route("/names")
+def get_all_names_and_addresses():
+    keys = redis.keys()
+    names_and_addresses = {key: redis.get(key) for key in keys}
+    return names_and_addresses
 
 @app.route("/<first_name>")
 def hello_name(first_name):
